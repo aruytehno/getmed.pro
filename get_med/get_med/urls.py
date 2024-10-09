552e50ed49607d00cb4739b692b5d85c30b3faec
aruytehno <aruytehno@yandex.ru>
2024-10-09 14:13:03 +0300

    Улучшил логику поведения сайта. Добавил кастомную авторизацию.

--- get_med/get_med/urls.py
+++ get_med/get_med/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', views.home, name='home'),  # Главная страница
     path('account/', views.account, name='account'),  # Личный кабинет пользователя
     path('register/', views.register, name='register'),  # Регистрация
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),  # Вход
+    path('login/', views.login_view, name='login'),  # Вход
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),  # Выход
 ]
--- get_med/users/views.py
+++ get_med/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.views.decorators.http import require_POST
 from .forms import RegisterForm
 from django.contrib.auth import logout
@@ -25,9 +25,28 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()  # Сохраняем нового пользователя
-            login(request, user)  # Логиним пользователя сразу после регистрации
-            return redirect('home')  # Перенаправляем на главную страницу
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Перенаправление на главную страницу после успешной регистрации
     else:
+        if request.user.is_authenticated:
+            return redirect('home')  # Перенаправление, если пользователь уже авторизован
         form = RegisterForm()
     return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Перенаправление на главную страницу после успешного входа
+        else:
+            error_message = "Неверные учетные данные."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        if request.user.is_authenticated:
+            return redirect('home')  # Перенаправление, если пользователь уже авторизован
+        return render(request, 'login.html')
