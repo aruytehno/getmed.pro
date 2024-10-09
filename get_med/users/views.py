e21654d14a9b47cbca79eef37076352f11894dca
aruytehno <aruytehno@yandex.ru>
2024-10-09 13:10:24 +0300

    Отладил авторизацию

--- get_med/users/views.py
+++ get_med/users/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.views.decorators.http import require_POST
+
 from .forms import RegisterForm
+from django.contrib.auth import logout
 
 def home(request):
     return render(request, 'home.html')
+
+@require_POST
+def logout_view(request):
+    logout(request)  # Выход из системы
+    return redirect('home')  # Перенаправление на главную страницу
 
 @login_required
 def account(request):
